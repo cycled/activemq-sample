4ae391cfdffa7a1b93e1e7611ff66697f758e156
wangchao <wangchaobjdx@163.com>
2016-06-10 03:01:57 +0800

    提交，注释

--- activemq-01/src/main/java/com/ljt/sample/activemq/queue/implementor/MapMessageQueueReceiver.java
+++ activemq-01/src/main/java/com/ljt/sample/activemq/queue/implementor/MapMessageQueueReceiver.java
@@ -8,6 +8,17 @@ import javax.jms.Session;
 
 import com.ljt.sample.activemq.queue.AbstractQueue;
 
+/**
+ * @Project       : activemq-01
+ * @Program Name  : com.ljt.sample.activemq.queue.implementor.MapMessageQueueReceiver.java
+ * @Description   : 创建一个MapMessage类型的消息队列接收者
+ * @Author        : wangchao
+ * @Creation Date : 2016年6月10日 上午2:57:43 
+ * @ModificationHistory  
+ * Who          When             What 
+ * ----------   -------------    -----------------------------------
+ * wangchao     2016年6月10日        create
+ */
 public class MapMessageQueueReceiver {
 	
 	public static void main(String[] args) throws JMSException {
